cfbc0493d16e294c353d6e7817740c3f00baadc4
Anh Thy Vu <anhthyvu26@gmail.com>
2024-10-18 22:45:47 -0400

    Need to link each page

---
+++ javascript/admin-dashboard.js
@@ -0,0 +1,33 @@
+//SideBar animation
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'none';
+});
+
+//Fill booking animations
+Bookings.forEach(booking => {
+    const tr = document.createElement('tr');
+    const trContent = `
+        <td>${booking.clientName}</td>
+        <td>${booking.service}</td>
+        <td>${booking.date}</td>
+        <td>${booking.time}</td>
+        <td class="${
+            booking.status === 'Pending' ? 'warning' :
+            booking.status === 'Delayed' ? 'danger' :
+            booking.status === 'Complete' ? 'success' :
+            'primary'
+        }">${booking.status}</td>
+        <td class="primary">Details</td>
+    `;
+
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr);
+});
---
+++ javascript/bookings.js
@@ -0,0 +1,57 @@
+const Bookings = [
+    {
+        clientName: 'John Doe',
+        service: 'Service 1',
+        date: 'Oct. 25, 2024',
+        time: '3:00 PM',
+        status: 'Pending'
+    },
+
+    {
+        clientName: 'Jane Doe',
+        service: 'Service 3',
+        date: 'Oct. 1, 2024',
+        time: '12:00 PM',
+        status: 'Complete'
+    },
+
+    {
+        clientName: 'Brad Smith',
+        service: 'Service 3',
+        date: 'Oct. 10, 2024',
+        time: '2:30 PM',
+        status: 'Delayed'
+    },
+
+    {
+        clientName: 'Holly Grant',
+        service: 'Service 2',
+        date: 'Nov. 16, 2024',
+        time: '2:30 PM',
+        status: 'Pending'
+    },
+
+    {
+        clientName: 'Lily West',
+        service: 'Service 3',
+        date: 'Oct. 16, 2024',
+        time: '9:30 PM',
+        status: 'Complete'
+    },
+
+    {
+        clientName: 'Sarah Knowles',
+        service: 'Service 1',
+        date: 'Oct. 30, 2024',
+        time: '10:30 PM',
+        status: 'Pending'
+    },
+
+    {
+        clientName: 'Theo Mannuel',
+        service: 'Service 3',
+        date: 'Oct. 8, 2024',
+        time: '11:30 PM',
+        status: 'Complete'
+    }
+]
