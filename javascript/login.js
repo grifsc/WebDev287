1b7198ab655b4be298c153936c4e84cdf8ad9d2a
Anh Thy Vu <anhthyvu26@gmail.com>
2024-10-29 23:19:14 -0400

    Final Login, missing some links

--- javascript/login.js
+++ javascript/login.js
@@ -1,3 +1,4 @@
+//Swap animation for the login and registration
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
@@ -9,3 +10,23 @@ registerLink.addEventListener('click', () => {
 loginLink.addEventListener('click', () => {
     wrapper.classList.remove('active');
 });
+
+//Check credentials for user-role detection
+function handleLogin(event) {
+    event.preventDefault(); 
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    for (const user of Users) {
+        if (user.email === email && user.password === password) {
+            if (user.role === 'admin') {
+                window.location.href = "/html/admin-dashboard.html"; // Redirect to admin dashboard
+            } else {
+                window.location.href = "/html/client-home.html"; // Redirect to client dashboard
+            }
+            return;
+        }
+    }
+
+    alert("Invalid email or password.");
+}
---
+++ javascript/sampleAccount.js
@@ -0,0 +1,17 @@
+const Users = [
+    {
+        role: "admin",
+        email: "admin@gmail.com",
+        password: "admin"
+    },
+    {
+        role: "client",
+        email: "john.doe@gmail.com",
+        password: "client123"
+    },
+    {
+        role: "client",
+        email: "jane.doe@gmail.com",
+        password: "client456"
+    }
+]
