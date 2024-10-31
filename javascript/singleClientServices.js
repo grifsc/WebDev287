e313ef3d1492cde701fc694039a5d2915c18328f
Anh Thy Vu <anhthyvu26@gmail.com>
2024-10-31 14:07:14 -0400

    Final Working Version

--- javascript/singleClientServices.js
+++ javascript/singleClientServices.js
@@ -1,48 +1,48 @@
 const Services = [
     {
-        service: "Service 1",
+        service: "Blastoise Car Cleaning",
         date: "Oct. 30, 2024",
         time: "3:00 PM",
         status: "Pending",
     },
 
     {
-        service: "Service 2",
+        service: "Primeape's 1h Boxing Lesson",
         date: "Oct. 18, 2024",
         time: "11:00 AM",
         status: "Completed",
     },
 
     {
-        service: "Service 3",
+        service: "Fishing with Cramorant",
         date: "Oct. 12, 2024",
         time: "2:00 PM",
         status: "Canceled",
     },
 
     {
-        service: "Service 1",
+        service: "Blastoise Car Cleaning",
         date: "Sept. 26, 2024",
         time: "10:00 AM",
         status: "Completed"
     },
 
     {
-        service: "Service 3",
+        service: "Fishing with Cramorant",
         date: "Aug. 28, 2024",
         time: "9:00 AM",
         status: "Completed"
     },
 
     {
-        service: "Service 1",
+        service: "Sylveon's Medical Care",
         date: "Aug. 9, 2024",
         time: "1:00 PM",
         status: "Canceled"
     },
 
     {
-        service: "Service 2",
+        service: "Jigglipuff Singing a Lullaby",
         date: "Jul. 30, 2024",
         time: "8:00 AM",
         status: "Completed"
