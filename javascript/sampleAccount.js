fcca12ae2a15673e57a4a1de96d272ae1cb8a339
Anh Thy Vu <anhthyvu26@gmail.com>
2024-10-30 21:26:47 -0400

    small fix

--- javascript/sampleAccount.js
+++ javascript/sampleAccount.js
@@ -1,17 +1,12 @@
 const Users = [
     {
         role: "admin",
-        email: "admin@gmail.com",
+        email: "matteo@hotmail.com",
         password: "admin"
     },
     {
         role: "client",
         email: "john.doe@gmail.com",
         password: "client123"
-    },
-    {
-        role: "client",
-        email: "jane.doe@gmail.com",
-        password: "client456"
     }
 ]
