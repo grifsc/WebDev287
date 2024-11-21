90d739a5b1e176bb6ba6dd4422f3beafa90547eb
Anh Thy Vu <anhthyvu26@gmail.com>
2024-11-21 18:29:32 -0500

    Finalized Display Service Page

--- public/javascript/displayServices.js
+++ public/javascript/displayServices.js
@@ -45,8 +45,51 @@ function loadServices() {
 
                 row.appendChild(serviceDiv);
             });
+
+            //Display all services title in footer
+            const footerServiceContainer = document.querySelector('.service-name');
+            footerServiceContainer.innerHTML = '';
+
+            services.forEach(service => {
+                const serviceP = document.createElement('p');
+                serviceP.innerHTML = service.name;
+                footerServiceContainer.appendChild(serviceP);
+            });
         })
         .catch(error => console.error('Error loading services:', error));
+
+        //Fetch the contact information for the footer
+        fetch('/contact-info/1')
+        .then(response => response.json())
+        .then(data => {
+            const contactContainer = document.querySelector('.contact-details');
+            contactContainer.innerHTML = `
+                <p><i class="fa-solid fa-location-dot"></i>${data.address}, ${data.city}, ${data.state}</p>    
+                <p><i class="fa-solid fa-phone"></i>${data.phone}</p>    
+                <p><i class="fa-solid fa-envelope"></i>${data.email}</p>    
+            `;
+        })
+        .catch(error => console.error('Error loading contact info:', error));
+
+        //Fetch the remaining information for the footer
+        fetch('/footer-info/1')
+        .then(response => response.json())
+        .then(data => {
+            //About us section in footer
+            const aboutUsContainer = document.querySelector('.about-us-content');
+            const aboutUsP = document.createElement('p');
+            aboutUsP.innerHTML = data.aboutUs;
+            aboutUsContainer.appendChild(aboutUsP);
+
+            //Socials link
+            const socialsContainer = document.querySelector('.socials-link');
+            socialsContainer.innerHTML = `
+                <a class="socials-btn" href="${data.facebook}" role="button"><i class="fa-brands fa-facebook"></i></a>
+                <a class="socials-btn" href="${data.twitter}" role="button"><i class="fa-brands fa-twitter"></i></a>
+                <a class="socials-btn" href="${data.instagram}" role="button"><i class="fa-brands fa-instagram"></i></a>
+            `;
+        })
+        .catch(error => console.error('Error loading contact info:', error));
 }
 
 // Initialize the service list
