e7072cf71a4ceb8c87704dd1d4f82e04d54072f7
Anh Thy Vu <anhthyvu26@gmail.com>
2024-11-22 04:47:29 -0500

    finalized navbar

--- public/javascript/navbar.js
+++ public/javascript/navbar.js
@@ -1,20 +1,3 @@
-//Fetching the logo and the website name
-function loadNavbar() {
-    fetch('/home-page-info/1')  // Fetch data for ID = 1
-        .then(response => response.json())
-        .then(data => {
-            const logoContainer = document.querySelector('.logo');
-            logoContainer.innerHTML = `
-                    <img src="${data.logo}" alt="Logo">
-                    <h1>${data.name}</h1>
-                `;
-        })
-        .catch(error => console.error('Error loading services:', error));
-}
-
-// Call the function when the page loads
-window.onload = loadNavbar;
-
 //Navbar toggle function
 const toggleBtn= document.querySelector('.toggle-btn');
 const navbar= document.querySelector('.header .navbar');
