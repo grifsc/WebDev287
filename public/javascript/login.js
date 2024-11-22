1d53306d8c5320bb6b1860dff0e6bb8b1e7aabe2
Anh Thy Vu <anhthyvu26@gmail.com>
2024-11-22 04:51:19 -0500

    Dynamic display login done'

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -30,3 +30,31 @@ function handleLogin(event) {
 
     alert("Invalid email or password.");
 }
+
+function loadLogin(){
+    fetch('/home-page-info/1') 
+    .then(response => response.json())
+    .then(data => {
+        const logoContainer = document.querySelector('.logo');
+        logoContainer.innerHTML = `
+                <img src="${data.logo}" alt="Logo">
+                <h1>${data.name}</h1>
+            `;
+    }).catch(error => console.error('Error loading logo info:', error));
+
+    fetch('/footer-info/1')
+        .then(response => response.json())
+        .then(data => {
+            //Socials link
+            const socialsContainer = document.querySelector('.socials-link');
+            socialsContainer.innerHTML = `
+                <a class="socials-btn" href="${data.facebook}" role="button"><i class="fa-brands fa-facebook"></i></a>
+                <a class="socials-btn" href="${data.twitter}" role="button"><i class="fa-brands fa-twitter"></i></a>
+                <a class="socials-btn" href="${data.instagram}" role="button"><i class="fa-brands fa-instagram"></i></a>
+            `;
+        })
+        .catch(error => console.error('Error loading footer info:', error));
+}
+
+//Initialize page
+window.onload = loadLogin;
