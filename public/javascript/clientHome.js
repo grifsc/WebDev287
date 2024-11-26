6f9a737bccc6488cb7c42319b8a8e6ef4519de0c
Anh Thy Vu <anhthyvu26@gmail.com>
2024-11-26 17:35:34 -0500

    Final Client Home

--- public/javascript/clientHome.js
+++ public/javascript/clientHome.js
@@ -1,5 +1,47 @@
 //Fetch the client recent bookings
 function loadHome(){
+    //Display the logo and the client name dynamically
+    fetch('/home-page-info/1')
+        .then(response => response.json())
+        .then(data => {
+            const logoContainer = document.querySelector('.logo');
+            logoContainer.innerHTML = `
+                <img src="${data.logo}" alt="Logo">
+            `;
+        })
+        .catch(error => console.error('Error loading logo:', error));
+
+    //Store the idea to get the name
+    let userID = null;
+
+    fetch('/clientbookings')
+        .then(response => response.json())
+        .then(bookings => {
+            const userIDSet = new Set();
+            bookings.forEach(booking => userIDSet.add(booking.clientID));
+
+            if (userIDSet.size === 1) {
+                userID = [...userIDSet][0]; 
+            } else {
+                console.error('Multiple or no user IDs found in bookings.');
+                throw new Error('Invalid user ID.');
+            }
+        })
+        .catch(error => console.error('Error loading client id:', error));
+
+    fetch('/users')
+        .then(response => response.json())
+        .then(users => {
+            const user = users.find(user => user.id === userID);
+
+            if (user) {
+                const container = document.getElementById('client-name');
+                container.textContent = `${user.first}`;
+            } else {
+                console.error('User not found.');
+            }
+        }).catch(error => console.error('Error loading client name:', error));
+
     fetch('/clientbookings')
         .then(response => response.json())
         .then(bookings => {
