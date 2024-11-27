2d3d6e8472adea62552fd1c5cac7e32be031ec02
Anh Thy Vu <anhthyvu26@gmail.com>
2024-11-26 20:21:49 -0500

    Final Admin Manage Service Page

--- public/javascript/admin-manage-services.js
+++ public/javascript/admin-manage-services.js
@@ -1,16 +1,23 @@
-//variables for side menu
-const sideMenu = document.querySelector("aside");
-const menuBtn = document.querySelector("#menu-btn");
-const closeBtn = document.querySelector("#close-btn");
+//Display the logo and the client name dynamically
+fetch('/home-page-info/1')
+.then(response => response.json())
+.then(data => {
+    const logoContainer = document.querySelector('.logo');
+    logoContainer.innerHTML = `
+        <img src="${data.logo}" alt="Logo">
+    `;
+})
+.catch(error => console.error('Error loading logo:', error));
 
-//side menu toggle
-menuBtn.addEventListener('click', () => {
-    sideMenu.style.display = 'block';
-});
-
-closeBtn.addEventListener('click', () => {
-    sideMenu.style.display = 'none';
-});
+fetch('/admin')
+.then(response => response.json())
+.then(users => {
+    const name = users.first;
+    const container = document.getElementById('admin-name');
+    if (container) {
+        container.textContent = name; 
+    }
+}).catch(error => console.error('Error loading client name:', error));
 
 //load users and bookings data
 let userNames = [];
